d583cafb339f0db38a38d43afee5d399d330b116
pervertedpokepuffs <aaadlima97@gmail.com>
2020-10-24 20:21:27 +0800

    rm angular default page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-materialoverlayform';
+  title = 'DEMO: angular-materialoverlayform';
 }
